fc30c2f9139541afc5380d4c29315e4c99011e9d
Mohd Arshan <110154295+arshansaifi150@users.noreply.github.com>
2023-09-03 00:40:23 +0000

    Data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // it is used to treat all JS code as newer version
+
+// alert(3+3); //can't be used in nodejs as it is can only be used in browser like this 
+
+//make sure your code is easily readable 
+
+
+let name = "Arshan";
+
+let age = 21;
+let isLoggesIn = true;
+
+// let num; undefined
+//  let state = null  means empty
+
+// types of data types
+//number => 2 to power 53
+// bigInt => big number
+//string => "" or '' qotes can be skip by \
+//boolean  => true  
+// null => standalone value
+// undefined => value not assigned yet
+//symbol => uniquness
+
+
+//objects
+// console.log(typeof ""); give output as string
+// console.log(typeof null); give output as object
+// console.log(typeof undefined);  give output as undefined only 
+
